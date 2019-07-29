4d13ede0f9f7124656dd32c2ddbbee4976b157e9
soata <heytoway@gmail.com>
2019-07-29 17:30:55 +0900

    fix: postinstall path

--- prep.js
+++ prep.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
-
 const fs = require('fs');
 const path = require('path');
 const rootDir = './node_modules/@mdi/svg/svg';
 const output = './png/';
 const conv = require('svg2img');
 
+if (!fs.existsSync('png')) fs.mkdirSync('png')
 
-fs.mkdir(output)
 fs.readdirSync(rootDir)
   .forEach(filename => {
     const dir = [rootDir,filename].join(path.sep)
